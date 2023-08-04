ac32407fb72df46af51b1c810fcd36294675bbd5
althmy <141419932+althmy@users.noreply.github.com>
2023-08-04 17:29:14 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+local config = {
+	["GAMEINFO"] = {
+		["display"] = "Game Information"
+	},
+
+	["BASICS"] = {
+		["display"] = "Game Basics"
+	},
+
+	["CREDITS"] = {
+		["display"] = "Credits"
+	},
+
+	["LOG"] = {
+		["display"] = "Changelog"
+	}
+}
+
+return config
